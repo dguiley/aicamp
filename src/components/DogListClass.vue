2b6f0d3f4c794397521f032a2ff8c4e4e60a33c3
dathan <dathan@wilde.agency>
2022-09-19 23:10:47 -0700

    Initial setup.

---
+++ src/components/DogListClass.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <ul>
+    <li v-for="dog in dogs">{{dog}}...</li>
+  </ul>
+</template>
+
+
+<script  lang="ts">
+import { useDogStore } from '@/stores/dog'
+import {Options, Vue} from 'vue-class-component'
+
+export default class DogListClass extends Vue {
+  dogs = [] as string[]
+
+  async mounted() {
+    this.dogs= await useDogStore().fetchDogs()
+  }
+}
+</script>
+  
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  border-bottom: 1px solid white;
+}
+</style>
